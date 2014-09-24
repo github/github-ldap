00e8a036918a89bfb83d1f3364c71d46ff6168f0
Matt Todd <chiology@gmail.com>
2014-09-23 19:32:46 -0700

    Doco base methods

--- lib/github/ldap/membership_validators/base.rb
+++ lib/github/ldap/membership_validators/base.rb
@@ -4,6 +4,10 @@ module GitHub
       class Base
         attr_reader :ldap, :groups
 
+        # Public: Instantiate new validator.
+        #
+        # - ldap:   GitHub::Ldap object
+        # - groups: Array of Net::LDAP::Entry group objects
         def initialize(ldap, groups)
           @ldap   = ldap
           @groups = groups
@@ -15,6 +19,9 @@ module GitHub
         # def perform(entry)
         # end
 
+        # Internal: Domains to search through.
+        #
+        # Returns an Array of GitHub::Ldap::Domain objects.
         def domains
           @domains ||= ldap.search_domains.map { |base| ldap.domain(base) }
         end
