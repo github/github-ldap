6502ab1026d43d631780322c819f4af547ee4366
Matt Todd <chiology@gmail.com>
2014-10-06 17:05:21 -0700

    Fix Group tests, add fixture group with missing members

--- test/group_test.rb
+++ test/group_test.rb
@@ -2,12 +2,12 @@ require_relative 'test_helper'
 
 class GitHubLdapGroupTest < GitHub::Ldap::Test
   def groups_domain
-    @ldap.domain("ou=groups,dc=github,dc=com")
+    @ldap.domain("ou=Groups,dc=github,dc=com")
   end
 
   def setup
     @ldap  = GitHub::Ldap.new(options)
-    @group = @ldap.group("cn=enterprise,ou=groups,dc=github,dc=com")
+    @group = @ldap.group("cn=ghe-users,ou=Groups,dc=github,dc=com")
   end
 
   def test_group?
@@ -21,34 +21,36 @@ class GitHubLdapGroupTest < GitHub::Ldap::Test
   end
 
   def test_subgroups
-    assert_equal 3, @group.subgroups.size
+    group = @ldap.group("cn=deeply-nested-group0.0,ou=Groups,dc=github,dc=com")
+    assert_equal 2, group.subgroups.size
   end
 
   def test_members_from_subgroups
-    assert_equal 4, @group.members.size
+    group = @ldap.group("cn=deeply-nested-group0.0,ou=Groups,dc=github,dc=com")
+    assert_equal 10, group.members.size
   end
 
   def test_all_domain_groups
     groups = groups_domain.all_groups
-    assert_equal 8, groups.size
+    assert_equal 27, groups.size
   end
 
   def test_filter_domain_groups
-    groups = groups_domain.filter_groups('devs')
+    groups = groups_domain.filter_groups('ghe-users')
     assert_equal 1, groups.size
   end
 
   def test_filter_domain_groups_limited
     groups = []
-    groups_domain.filter_groups('enter', size: 1) do |entry|
+    groups_domain.filter_groups('deeply-nested-group', size: 1) do |entry|
       groups << entry
     end
     assert_equal 1, groups.size
   end
 
   def test_filter_domain_groups_unlimited
-    groups = groups_domain.filter_groups('ent')
-    assert_equal 3, groups.size
+    groups = groups_domain.filter_groups('deeply-nested-group')
+    assert_equal 5, groups.size
   end
 
   def test_unknown_group
@@ -59,11 +61,11 @@ end
 
 class GitHubLdapLoopedGroupTest < GitHub::Ldap::Test
   def setup
-    @group = GitHub::Ldap.new(options).group("cn=enterprise,ou=groups,dc=github,dc=com")
+    @group = GitHub::Ldap.new(options).group("cn=recursively-nested-groups,ou=Groups,dc=github,dc=com")
   end
 
   def test_members_from_subgroups
-    assert_equal 4, @group.members.size
+    assert_equal 10, @group.members.size
   end
 end
 
@@ -73,10 +75,10 @@ class GitHubLdapMissingEntriesTest < GitHub::Ldap::Test
   end
 
   def test_load_right_members
-    assert_equal 3, @ldap.domain("cn=spaniards,ou=groups,dc=github,dc=com").bind[:member].size
+    assert_equal 3, @ldap.domain("cn=missing-users,ou=groups,dc=github,dc=com").bind[:member].size
   end
 
   def test_ignore_missing_member_entries
-    assert_equal 2, @ldap.group("cn=spaniards,ou=groups,dc=github,dc=com").members.size
+    assert_equal 2, @ldap.group("cn=missing-users,ou=groups,dc=github,dc=com").members.size
   end
 end
