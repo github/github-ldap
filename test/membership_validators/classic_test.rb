70142997d2a36aa33002cc56ee4c39d155bce4df
Matt Todd <chiology@gmail.com>
2014-10-08 01:59:34 -0700

    Update membership validator tests

--- test/membership_validators/classic_test.rb
+++ test/membership_validators/classic_test.rb
@@ -1,16 +1,10 @@
 require_relative '../test_helper'
 
 class GitHubLdapClassicMembershipValidatorsTest < GitHub::Ldap::Test
-  def self.test_server_options
-    { search_domains: "dc=github,dc=com",
-      user_fixtures: FIXTURES.join('github-with-subgroups.ldif').to_s
-    }
-  end
-
   def setup
-    @ldap      = GitHub::Ldap.new(options)
+    @ldap      = GitHub::Ldap.new(options.merge(search_domains: %w(dc=github,dc=com)))
     @domain    = @ldap.domain("dc=github,dc=com")
-    @entry     = @domain.user?('user1.1.1.1')
+    @entry     = @domain.user?('user1')
     @validator = GitHub::Ldap::MembershipValidators::Classic
   end
 
@@ -20,60 +14,38 @@ class GitHubLdapClassicMembershipValidatorsTest < GitHub::Ldap::Test
   end
 
   def test_validates_user_in_group
-    validator = make_validator(%w(group1.1.1.1))
+    validator = make_validator(%w(nested-group1))
     assert validator.perform(@entry)
   end
 
   def test_validates_user_in_child_group
-    validator = make_validator(%w(group1.1.1))
+    validator = make_validator(%w(n-depth-nested-group1))
     assert validator.perform(@entry)
   end
 
   def test_validates_user_in_grandchild_group
-    validator = make_validator(%w(group1.1))
+    validator = make_validator(%w(n-depth-nested-group2))
     assert validator.perform(@entry)
   end
 
   def test_validates_user_in_great_grandchild_group
-    validator = make_validator(%w(group1))
+    validator = make_validator(%w(n-depth-nested-group3))
     assert validator.perform(@entry)
   end
 
   def test_does_not_validate_user_not_in_group
-    validator = make_validator(%w(Enterprise))
+    validator = make_validator(%w(ghe-admins))
     refute validator.perform(@entry)
   end
 
   def test_does_not_validate_user_not_in_any_group
-    @entry = @domain.user?('admin')
-    validator = make_validator(%w(Enterprise))
+    @entry = @domain.user?('groupless-user1')
+    validator = make_validator(%w(all-users))
     refute validator.perform(@entry)
   end
-end
 
-class GitHubLdapClassicMembershipValidatorsWithPosixGroupsTest < GitHub::Ldap::Test
-  def self.test_server_options
-    { search_domains: "dc=github,dc=com",
-      uid: "uid",
-      custom_schemas: FIXTURES.join('posixGroup.schema.ldif'),
-      user_fixtures: FIXTURES.join('github-with-posixGroups.ldif').to_s
-    }
-  end
-
-  def setup
-    @ldap      = GitHub::Ldap.new(options)
-    @domain    = @ldap.domain("dc=github,dc=com")
-    @entry     = @domain.user?('user1.1.1.1')
-    @validator = GitHub::Ldap::MembershipValidators::Classic
-  end
-
-  def make_validator(groups)
-    groups = @domain.groups(groups)
-    @validator.new(@ldap, groups)
-  end
-
-  def test_validates_user_in_group
-    validator = make_validator(%w(group1.1.1.1))
+  def test_validates_user_in_posix_group
+    validator = make_validator(%w(posix-group1))
     assert validator.perform(@entry)
   end
 end
