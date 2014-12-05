cb8f4918b364b1fa41e713d74a115ac791b4352f
Matt Todd <chiology@gmail.com>
2014-12-05 14:57:49 -0800

    Remove Detect meta-strategy, inline config/detection

--- lib/github/ldap/member_search.rb
+++ lib/github/ldap/member_search.rb
@@ -1,26 +1,4 @@
 require 'github/ldap/member_search/base'
-require 'github/ldap/member_search/detect'
 require 'github/ldap/member_search/classic'
 require 'github/ldap/member_search/recursive'
 require 'github/ldap/member_search/active_directory'
-
-module GitHub
-  class Ldap
-    # Provides various strategies for member lookup.
-    #
-    # For example:
-    #
-    #   group = domain.groups(%w(Engineering)).first
-    #   strategy = GitHub::Ldap::MemberSearch::Recursive.new(ldap)
-    #   strategy.perform(group) #=> [#<Net::LDAP::Entry>]
-    #
-    module MemberSearch
-      # Internal: Mapping of strategy name to class.
-      STRATEGIES = {
-        :classic          => GitHub::Ldap::MemberSearch::Classic,
-        :recursive        => GitHub::Ldap::MemberSearch::Recursive,
-        :active_directory => GitHub::Ldap::MemberSearch::ActiveDirectory
-      }
-    end
-  end
-end
--- lib/github/ldap/membership_validators.rb
+++ lib/github/ldap/membership_validators.rb
@@ -1,26 +1,4 @@
 require 'github/ldap/membership_validators/base'
-require 'github/ldap/membership_validators/detect'
 require 'github/ldap/membership_validators/classic'
 require 'github/ldap/membership_validators/recursive'
 require 'github/ldap/membership_validators/active_directory'
-
-module GitHub
-  class Ldap
-    # Provides various strategies for validating membership.
-    #
-    # For example:
-    #
-    #   groups = domain.groups(%w(Engineering))
-    #   validator = GitHub::Ldap::MembershipValidators::Classic.new(ldap, groups)
-    #   validator.perform(entry) #=> true
-    #
-    module MembershipValidators
-      # Internal: Mapping of strategy name to class.
-      STRATEGIES = {
-        :classic          => GitHub::Ldap::MembershipValidators::Classic,
-        :recursive        => GitHub::Ldap::MembershipValidators::Recursive,
-        :active_directory => GitHub::Ldap::MembershipValidators::ActiveDirectory
-      }
-    end
-  end
-end
