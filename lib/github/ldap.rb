50af684474771ddfda41e2aa44ae4cb5136ae066
Dave Sims <davesims@github.com>
2016-07-08 19:11:16 -0500

    Require forest_search

--- lib/github/ldap.rb
+++ lib/github/ldap.rb
@@ -9,6 +9,7 @@ require 'github/ldap/virtual_group'
 require 'github/ldap/virtual_attributes'
 require 'github/ldap/instrumentation'
 require 'github/ldap/member_search'
+require 'github/ldap/forest_search'
 require 'github/ldap/membership_validators'
 
 module GitHub
