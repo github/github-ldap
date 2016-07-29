54defdb2b7777ea178ef853f9a6163e38bf46716
Dave Sims <davesims@github.com>
2016-07-29 13:24:57 -0500

    Create a ConnectionPool object to encapsulate caching connection objects

---
+++ test/connection_pool_test.rb
@@ -0,0 +1,21 @@
+require_relative 'test_helper'
+require 'mocha/mini_test'
+
+class GitHubLdapConnectionPoolTestCases < GitHub::Ldap::Test
+
+  def test_get_connection
+    conn = GitHub::Ldap::ConnectionPool.get_connection({:host => "host"})
+    assert_equal GitHub::Ldap, conn.class
+  end
+
+  def test_returns_cached_connection
+    conn1 = GitHub::Ldap::ConnectionPool.get_connection({:host => "ghe.local"})
+    conn2 = GitHub::Ldap::ConnectionPool.get_connection({:host => "ghe.local"})
+    assert_equal conn1.object_id, conn2.object_id
+
+    conn3 = GitHub::Ldap::ConnectionPool.get_connection({:host => "ghe.dev"})
+    conn4 = GitHub::Ldap::ConnectionPool.get_connection({:host => "ghe.dev"})
+    refute_equal conn1.object_id, conn3.object_id
+    assert_equal conn3.object_id, conn4.object_id
+  end
+end
