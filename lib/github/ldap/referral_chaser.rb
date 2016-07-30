b7da6e7605b2034da638cc192fa3d81b3af78a56
Dave Sims <davesims@github.com>
2016-07-30 13:09:45 -0500

    Pushing referral aggregation from callsite into ReferallChaser

--- lib/github/ldap/referral_chaser.rb
+++ lib/github/ldap/referral_chaser.rb
@@ -2,23 +2,33 @@ module GitHub
   class Ldap
     class ReferralChaser
 
-      def initialize(referral_entries, admin_user, admin_password)
-        @referral_entries = referral_entries
-        @admin_user = admin_user
-        @admin_password = admin_password
+      def initialize(connection)
+        @connection = connection
+        @admin_user = connection.admin_user
+        @admin_password = connection.admin_password
       end
 
-      def with_referrals
+      def search(options)
+        search_results = []
+        referral_entries = []
+
+        search_results = connection.search(options) do |referral_entry|
+          referral_entries << referral_entry
+        end
+
         referral_entries.each do |entry|
           entry[:search_referrals].each do |referral_string|
-            yield(Referral.new(referral_string, admin_user, admin_password))
+            referral = Referral.new(referral_string, admin_user, admin_password)
+            search_results.concat(referral.search(options))
           end
         end
+
+        search_results
       end
 
       private
 
-      attr_reader :referral_entries, :admin_user, :admin_password
+      attr_reader :connection, :admin_user, :admin_password
 
       class Referral
         def initialize(referral_string, admin_user, admin_password)
