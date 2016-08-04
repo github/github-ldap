7cdbe8650ff852d82e9a1b12b9efa6ae7416b86a
Dave Sims <davesims@github.com>
2016-08-04 14:51:21 -0500

    Minor style/cleanup

--- lib/github/ldap/membership_validators/active_directory.rb
+++ lib/github/ldap/membership_validators/active_directory.rb
@@ -24,23 +24,18 @@ module GitHub
           # Sets the entry to the base and scopes the search to the base,
           # according to the source documentation, found here:
           # http://msdn.microsoft.com/en-us/library/aa746475(v=vs.85).aspx
-
-          filter = membership_in_chain_filter(entry)
-          options = {
-            filter: filter,
+          #
+          # Use ReferralChaser to chase any potential referrals for an entry that may be owned by a different
+          # domain controller.
+          matched = referral_chaser.search \
+            filter: membership_in_chain_filter(entry),
             base:   entry.dn,
             scope:  Net::LDAP::SearchScope_BaseObject,
             return_referrals: true,
             attributes: ATTRS
-          }
-
-          # Chase any potential referrals for an entry that may be owned by a different
-          # domain controller.
-          matched = referral_chaser.search(options)
 
           # membership validated if entry was matched and returned as a result
           # Active Directory DNs are case-insensitive
-
           result = Array(matched).map { |m| m.dn.downcase }.include?(entry.dn.downcase)
         end
 
