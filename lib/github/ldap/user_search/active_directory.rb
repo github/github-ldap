3da33e4fa099d2937fce3d4d705cbe7dbcaf908d
Dave Sims <davesims@github.com>
2016-08-04 12:02:39 -0500

    Make global connection interface private

--- lib/github/ldap/user_search/active_directory.rb
+++ lib/github/ldap/user_search/active_directory.rb
@@ -3,20 +3,16 @@ module GitHub
     module UserSearch
       class ActiveDirectory < Default
 
-        # Returns a connection to the Active Directory Global Catalog
-        #
-        # See: https://technet.microsoft.com/en-us/library/cc728188(v=ws.10).aspx
-        #
-        def global_catalog_connection
-          GlobalCatalog.connection(ldap)
-        end
-
         private
 
         # Private - Overridden from base class to set the base to "", and use the
         # Global Catalog to perform the user search.
         def search(search_options)
           Array(global_catalog_connection.search(search_options.merge(options)))
+        end
+
+        def global_catalog_connection
+          GlobalCatalog.connection(ldap)
         end
 
         # When doing a global search for a user's DN, set the search base to blank
@@ -29,6 +25,10 @@ module GitHub
         STANDARD_GC_PORT = 3268
         LDAPS_GC_PORT = 3269
 
+        # Returns a connection to the Active Directory Global Catalog
+        #
+        # See: https://technet.microsoft.com/en-us/library/cc728188(v=ws.10).aspx
+        #
         def self.connection(ldap)
           @global_catalog_instance ||= begin
             netldap = ldap.connection
