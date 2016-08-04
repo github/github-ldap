a4573d71891f98f13de89461028e8ad4fbcdff21
Dave Sims <davesims@github.com>
2016-08-04 14:40:26 -0500

    Remove test file; clean up merge

--- lib/github/ldap.rb
+++ lib/github/ldap.rb
@@ -300,7 +300,6 @@ module GitHub
         end
     end
 
-<<<<<<< 96ba488c1e07d7a1f95f1287414805c3b0493b4c
     # Internal:  Set the user search strategy that will be used by
     #            Domain#user?.
     #
