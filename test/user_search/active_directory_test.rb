88319aa24b6d000c00cf68d49d7d5b0dae0d568c
Dave Sims <davesims@github.com>
2016-08-04 23:28:54 -0500

    CI Fixes: move all setup code inline

--- test/user_search/active_directory_test.rb
+++ test/user_search/active_directory_test.rb
@@ -2,37 +2,44 @@ require_relative '../test_helper'
 
 class GitHubLdapActiveDirectoryUserSearchTests < GitHub::Ldap::Test
 
-  def setup
-    @ldap = GitHub::Ldap.new(options.merge(host: 'ghe.dev'))
-    @ad_user_search = GitHub::Ldap::UserSearch::ActiveDirectory.new(@ldap)
-  end
-
   def test_global_catalog_returns_empty_array_for_no_results
+    ldap = GitHub::Ldap.new(options.merge(host: 'ghe.dev'))
+    ad_user_search = GitHub::Ldap::UserSearch::ActiveDirectory.new(ldap)
+
     mock_global_catalog_connection = mock("GitHub::Ldap::UserSearch::GlobalCatalog")
     mock_global_catalog_connection.expects(:search).returns(nil)
-    @ad_user_search.expects(:global_catalog_connection).returns(mock_global_catalog_connection)
-    results = @ad_user_search.perform("login", "CN=Joe", "uid", {})
+    ad_user_search.expects(:global_catalog_connection).returns(mock_global_catalog_connection)
+    results = ad_user_search.perform("login", "CN=Joe", "uid", {})
     assert_equal [], results
   end
 
   def test_global_catalog_returns_array_of_results
+    ldap = GitHub::Ldap.new(options.merge(host: 'ghe.dev'))
+    ad_user_search = GitHub::Ldap::UserSearch::ActiveDirectory.new(ldap)
+
     mock_global_catalog_connection = mock("GitHub::Ldap::UserSearch::GlobalCatalog")
     stub_entry = mock("Net::LDAP::Entry")
+
     mock_global_catalog_connection.expects(:search).returns(stub_entry)
-    @ad_user_search.expects(:global_catalog_connection).returns(mock_global_catalog_connection)
-    results = @ad_user_search.perform("login", "CN=Joe", "uid", {})
+    ad_user_search.expects(:global_catalog_connection).returns(mock_global_catalog_connection)
+
+    results = ad_user_search.perform("login", "CN=Joe", "uid", {})
     assert_equal [stub_entry], results
   end
 
   def test_searches_with_empty_base_dn
+    ldap = GitHub::Ldap.new(options.merge(host: 'ghe.dev'))
+    ad_user_search = GitHub::Ldap::UserSearch::ActiveDirectory.new(ldap)
+
     mock_global_catalog_connection = mock("GitHub::Ldap::UserSearch::GlobalCatalog")
     mock_global_catalog_connection.expects(:search).with(has_entry(:base => ""))
-    @ad_user_search.expects(:global_catalog_connection).returns(mock_global_catalog_connection)
-    @ad_user_search.perform("login", "CN=Joe", "uid", {})
+    ad_user_search.expects(:global_catalog_connection).returns(mock_global_catalog_connection)
+    ad_user_search.perform("login", "CN=Joe", "uid", {})
   end
 
   def test_global_catalog_default_settings
-    global_catalog = GitHub::Ldap::UserSearch::GlobalCatalog.connection(@ldap)
+    ldap = GitHub::Ldap.new(options.merge(host: 'ghe.dev'))
+    global_catalog = GitHub::Ldap::UserSearch::GlobalCatalog.connection(ldap)
     instrumentation_service = global_catalog.instance_variable_get(:@instrumentation_service)
 
     auth = global_catalog.instance_variable_get(:@auth)
