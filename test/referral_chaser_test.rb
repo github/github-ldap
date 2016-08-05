2b8005820670574b9d4d03381d8c808c9fff04c4
Matt Todd <chiology@gmail.com>
2016-08-05 09:42:33 -0400

    Rename mock_connection to ldap
    Stylistic preference to be more consistent with other tests.

--- test/referral_chaser_test.rb
+++ test/referral_chaser_test.rb
@@ -3,12 +3,12 @@ require_relative 'test_helper'
 class GitHubLdapReferralChaserTestCases < GitHub::Ldap::Test
 
   def setup
-    @mock_connection = GitHub::Ldap.new(options)
-    @chaser = GitHub::Ldap::ReferralChaser.new(@mock_connection)
+    @ldap = GitHub::Ldap.new(options)
+    @chaser = GitHub::Ldap::ReferralChaser.new(@ldap)
   end
 
   def test_creates_referral_with_connection_credentials
-    @mock_connection.expects(:search).yields({ search_referrals: ["referral string"]}).returns([])
+    @ldap.expects(:search).yields({ search_referrals: ["referral string"]}).returns([])
 
     referral = mock("GitHub::Ldap::ReferralChaser::Referral")
     referral.stubs(:search).returns([])
@@ -21,19 +21,19 @@ class GitHubLdapReferralChaserTestCases < GitHub::Ldap::Test
   end
 
   def test_creates_referral_with_default_port
-    @mock_connection.expects(:search).yields({
+    @ldap.expects(:search).yields({
       search_referrals: ["ldap://dc1.ghe.local/CN=Maggie%20Mae,CN=Users,DC=dc4,DC=ghe,DC=local"]
     }).returns([])
 
     stub_referral_connection = mock("GitHub::Ldap")
     stub_referral_connection.stubs(:search).returns([])
     GitHub::Ldap::ConnectionCache.expects(:get_connection).with(has_entry(port: options[:port])).returns(stub_referral_connection)
-    chaser = GitHub::Ldap::ReferralChaser.new(@mock_connection)
+    chaser = GitHub::Ldap::ReferralChaser.new(@ldap)
     chaser.search({})
   end
 
   def test_creates_referral_for_first_referral_string
-    @mock_connection.expects(:search).multiple_yields([
+    @ldap.expects(:search).multiple_yields([
       { search_referrals:
         ["ldap://dc1.ghe.local/CN=Maggie%20Mae,CN=Users,DC=dc4,DC=ghe,DC=local",
          "ldap://dc2.ghe.local/CN=Maggie%20Mae,CN=Users,DC=dc4,DC=ghe,DC=local"]
@@ -60,7 +60,7 @@ class GitHubLdapReferralChaserTestCases < GitHub::Ldap::Test
   end
 
   def test_returns_referral_search_results
-    @mock_connection.expects(:search).multiple_yields([
+    @ldap.expects(:search).multiple_yields([
       { search_referrals:
         ["ldap://dc1.ghe.local/CN=Maggie%20Mae,CN=Users,DC=dc4,DC=ghe,DC=local",
          "ldap://dc2.ghe.local/CN=Maggie%20Mae,CN=Users,DC=dc4,DC=ghe,DC=local"]
@@ -82,7 +82,7 @@ class GitHubLdapReferralChaserTestCases < GitHub::Ldap::Test
   end
 
   def test_returns_referral_search_results
-    @mock_connection.expects(:search).yields({ foo: ["not a referral"] })
+    @ldap.expects(:search).yields({ foo: ["not a referral"] })
 
     GitHub::Ldap::ReferralChaser::Referral.expects(:new).never
     results = @chaser.search({})
