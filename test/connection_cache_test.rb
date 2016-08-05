a0f1f24150cb14eaab812be653ffb938bf66d632
Dave Sims <davesims@github.com>
2016-08-04 23:13:28 -0500

    Fixing CI: don't use a real connection

--- test/connection_cache_test.rb
+++ test/connection_cache_test.rb
@@ -2,19 +2,19 @@ require_relative 'test_helper'
 
 class GitHubLdapConnectionCacheTestCases < GitHub::Ldap::Test
 
-  def test_get_connection
-    conn = GitHub::Ldap::ConnectionCache.get_connection({:host => "host"})
-    assert_equal GitHub::Ldap, conn.class
+  def setup
+    mock_ldap = mock("GitHub::Ldap")
+    GitHub::Ldap.stubs(:new).returns(mock_ldap)
   end
 
   def test_returns_cached_connection
-    conn1 = GitHub::Ldap::ConnectionCache.get_connection({:host => "ghe.local"})
-    conn2 = GitHub::Ldap::ConnectionCache.get_connection({:host => "ghe.local"})
+    conn1 = GitHub::Ldap::ConnectionCache.get_connection({:host => "ghe.gh"})
+    conn2 = GitHub::Ldap::ConnectionCache.get_connection({:host => "ghe.gh"})
     assert_equal conn1.object_id, conn2.object_id
   end
 
   def test_creates_new_connections_per_host
-    conn1 = GitHub::Ldap::ConnectionCache.get_connection({:host => "ghe.local"})
+    conn1 = GitHub::Ldap::ConnectionCache.get_connection({:host => "ghe.gh"})
     conn2 = GitHub::Ldap::ConnectionCache.get_connection({:host => "ghe.dev"})
     conn3 = GitHub::Ldap::ConnectionCache.get_connection({:host => "ghe.dev"})
     refute_equal conn1.object_id, conn2.object_id
